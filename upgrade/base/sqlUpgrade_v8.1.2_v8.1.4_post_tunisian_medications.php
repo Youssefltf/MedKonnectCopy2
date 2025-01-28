4e9a45569e63a9b31eabfa3f6e12a4e26d4d30e2
Mohamed <msi.moussi@gmail.com>
2025-01-28 18:24:38 +0100

    - Adaptation de script php pour construire les valeurs de champ voiesAdmin

--- upgrade/base/sqlUpgrade_v8.1.2_v8.1.4_post_tunisian_medications.php
+++ upgrade/base/sqlUpgrade_v8.1.2_v8.1.4_post_tunisian_medications.php
@@ -89,6 +89,9 @@ try {
 			continue;
 		}
 
+		// Déterminer la voie d'administration
+		$voieAdmin = determineVoieAdmin($forme, $nom);
+
 		// Handle duplicates
 		if (!isset($occurrences[$codeCIS])) {
 			$occurrences[$codeCIS] = 0;
@@ -104,6 +107,7 @@ try {
 			$modifiedCodeCIS,
 			$denomination,
 			substr($forme, 0, 255),
+			$voieAdmin,
 			$dateAMM,
 			'Autorisation active',
 			'Commercialisée',
@@ -188,16 +192,102 @@ try {
 }
 
 /**
- * Insert rows in batches into the database.
+ * Détermine la voie d'administration en fonction de la forme pharmaceutique.
+ */
+function determineVoieAdmin($formePharma, $denomination)
+{
+	$formePharma = strtolower($formePharma);
+	$denomination = strtolower($denomination);
+
+	$mappingVoies = [
+		"orale" => [
+			"comprimé", "comprimé pelliculé", "comprimé pelliculé sécable", "comprimé orodispersible",
+			"comprimé dispersible sécable", "comprimé à libération prolongée", "gélule",
+			"gélule gastrorésistante", "granulés pour solution buvable", "sirop",
+			"suspension buvable", "solution buvable", "comprimé effervescent",
+			"comprimé à croquer", "poudre pour solution orale", "granules effervescents",
+			"poudre orale", "capsule molle", "ampoules buvables", "comprimé pour suspension buvable",
+			"pastilles", "comprime enrobé", "gouttes buvables", "granulés à libération prolongée",
+			"granules"
+		],
+		"intraveineuse" => [
+			"solution pour perfusion", "injection iv", "intraveineuse", "perfus",
+			"solution injectable pour perfusion", "poudre pour solution pour perfusion",
+			"poudre lyophilisée à diluer pour perfusion", "emulsion injectable pour perfusion",
+			"pdre p.prep.injectable", "poudre pour suspension injectable"
+		],
+		"intramusculaire" => [
+			"solution injectable", "ampoule im", "intramusculaire",
+			"solution injectable i.m et buvable", "poudre pour suspension injectable",
+			"suspension injectable", "émulsion injectable"
+		],
+		"sous-cutanée" => [
+			"injection sc", "seringue préremplie sc"
+		],
+		"cutanée" => [
+			"crème dermique", "pommade dermique", "gel dermique", "lotion",
+			"shampooing", "poudre à usage externe", "baume", "liniment",
+			"gel moussant", "spray solution", "lait dermique", "patch", "émulsion",
+			"solution externe", "crème dermique"
+		],
+		"ophtalmique" => [
+			"collyre", "collyre monodose", "pommade ophtalmique",
+			"solution ophtalmique", "gel ophtalmique"
+		],
+		"nasale" => [
+			"spray nasal", "solution pour pulvérisation nasale", "solution nasale",
+			"gouttes nasales", "suspension nasale"
+		],
+		"pulmonaire" => [
+			"aérosol", "inhalateur", "poudre pour inhalation buccale",
+			"suspension pour inhalation", "liquide volatil pour inhalation",
+			"liquide pour inhalation", "solution pour pulvérisation buccale",
+			"solution pour inhalation"
+		],
+		"rectale" => [
+			"suppositoire", "suppositoire sécable", "gel rectal", "pommade rectale",
+			"suspension rectale", "solution rectale", "mousse rectale"
+		],
+		"vaginale" => [
+			"ovule", "crème vaginale", "gel vaginal",
+			"capsules gynécologiques", "solution gynécologique", "gélule vaginale"
+		],
+		"transdermique" => [
+			"dispositif trans-dermique", "emplâtre", "patch transdermique"
+		],
+		"auriculaire" => [
+			"gouttes auriculaires", "poudre auriculaire"
+		],
+		"autre spécifique" => [
+			"implant", "perfusion intrathécale", "intra-articulaire",
+			"suspension pour administration intravesicale", "solution intra-oculaire",
+			"doses homéopathiques", "globules", "gel urétral",
+			"susp. p. instill endotrachéobronchique", "soluté pour dialyse péritonéale",
+			"préparation viscoélastique", "émulsion injectable"
+		]
+	];
+
+	foreach ($mappingVoies as $voie => $motsCles) {
+		foreach ($motsCles as $motCle) {
+			if (strpos($formePharma, $motCle) !== false || strpos($denomination, $motCle) !== false) {
+				return $voie;
+			}
+		}
+	}
+
+	return "inconnue";
+}
+
+/**
+ * Insère les données dans la base en batch.
  */
 function insertBatch(PDO $pdo, array $batchData)
 {
 	$sql = "
-        INSERT INTO bdpm_specialites_tn (codeCIS, denomination, formePharma, dateAMM, statutAdminAMM, etatCommercialisation, tituAMM, DCI)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO bdpm_specialites_tn (codeCIS, denomination, formePharma, voiesAdmin, dateAMM, statutAdminAMM, etatCommercialisation, tituAMM, DCI)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
     ";
 	$stmt = $pdo->prepare($sql);
-
 	foreach ($batchData as $data) {
 		$stmt->execute($data);
 	}
